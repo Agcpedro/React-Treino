b099501a35127a9bd9175089c2d359d7c0307213
Agcpedro <101598985+Agcpedro@users.noreply.github.com>
2024-05-21 14:38:23 -0300

    Add files via upload

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { MouseEventHandler } from "react";
+
+interface Props {
+  children: string;
+  color?: "primary" | "secundary " | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  //Hook: Estamos indicando ao react que esse componente dentro do useState, vai te data ou estados que vão mudar ao longo do tempo
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
